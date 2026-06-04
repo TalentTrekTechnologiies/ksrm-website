34e0035b88c615971aa53bc6c2e499cb6209c3cd
Chandu Paramkusam <chanduparamkusam@gmail.com>
2026-06-04 19:08:24 +0530

    Complete homepage, header, footer, intro animation

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,10 +1,23 @@
-import Image from "next/image";
+import Hero from "@/components/home/Hero"
+import StatsBar from "@/components/home/StatsBar"
+import AboutPreview from "@/components/home/AboutPreview"
+import CampusServices from "@/components/home/CampusServices"
+import Departments from "@/components/home/Departments"
+import LatestNews from "@/components/home/LatestNews"
+import Placements from "@/components/home/Placements"
+import Accreditation from "@/components/home/Accreditation"
 
 export default function Home() {
   return (
     <main>
-      <h1>KSRM College of Engineering</h1>
-      <p>Website is being built</p>
+      <Hero />
+      <StatsBar />
+      <AboutPreview />
+      <CampusServices />
+      <Departments />
+      <LatestNews />
+      <Placements />
+      <Accreditation />
     </main>
   )
 }
